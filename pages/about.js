03b1b2fca49e74144d0efe42652a668e92dbadf7
kenkomu <komukenneth216@gmail.com>
2023-07-13 12:13:24 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About </title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>
+                    About
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla omnis molestiae expedita iusto dicta nam eum sint magnam reprehenderit, quod earum totam, aliquid non qui vitae odit dignissimos, ea minus.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur aperiam facilis, hic excepturi, velit nemo alias architecto animi voluptatum et, ea nobis placeat aliquam fuga quam odio cumque cupiditate dolorum?</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
